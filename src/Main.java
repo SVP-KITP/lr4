6d46af7b7e8b75414d5487e336ca62b500b37366
SVP-KITP <svp_kitp@mail.ru>
2017-12-06 14:54:38 +0300

    "3th Commit"

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,8 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("Second Commit");
 
+
+        System.out.println("Changes on IDE Intellij Idea");
+
     }
 }
